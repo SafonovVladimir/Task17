5ef6de9785aebdba1df1bad640d38438806def4b
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2022-11-05 11:11:22 +0200

    1.1.0

--- Task17/celery.py
+++ Task17/celery.py
@@ -12,27 +12,27 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.beat_schedule = {
     'every-1-hour': {
         'task': 'weather.tasks.send_email',
-        "schedule": crontab(minute="*/60"),
+        "schedule": crontab(minute='0', hour='*/1'),
         'args': ('1H',)
     },
     'every-3-hours': {
         'task': 'weather.tasks.send_email',
-        "schedule": crontab(hour="*/3"),
+        "schedule": crontab(minute='0', hour='*/3'),
         'args': ('3H',)
     },
     'every-6-hours': {
         'task': 'weather.tasks.send_email',
-        "schedule": crontab(hour="*/6"),
+        "schedule": crontab(minute='0', hour='*/6'),
         'args': ('6H',)
     },
     'every-12-hours': {
         'task': 'weather.tasks.send_email',
-        "schedule": crontab(hour="*/12"),
+        "schedule": crontab(minute='0', hour='*/12'),
         'args': ('12H',)
     },
     'every-24-hours': {
         'task': 'weather.tasks.send_email',
-        "schedule": crontab(hour="*/24"),
+        "schedule": crontab(minute='0', hour='*/24'),
         'args': ('24H',)
     },
 }
